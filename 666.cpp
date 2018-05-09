c90028ec328220ac6ac2baffb307962e52260e55
LucienShui <18952904@qq.com>
2018-05-09 13:23:34 +0800

    0.0.1

---
+++ 666.cpp
@@ -0,0 +1,14 @@
+#include "666.h"
+#include "ui_666.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
